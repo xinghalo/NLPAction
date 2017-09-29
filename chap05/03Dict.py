aaaadb3a17cdbe1e6ef2047d1c8f710fceed08e2
xinghalo <xinghalo@163.com>
2017-09-29 21:07:07 +0800

    词典

---
+++ chap05/03Dict.py
@@ -0,0 +1,20 @@
+#-*- coding: UTF-8 -*-
+from __future__ import division
+
+import nltk
+import re, pprint
+import os
+from urllib import urlopen
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+alice = nltk.corpus.gutenberg.words('carroll-alice.txt')
+vocab = nltk.FreqDist(alice)
+v1000 = list(vocab)[:1000]
+mapping = nltk.defaultdict(lambda: 'UNK')
+for v in v1000:
+    mapping[v] = v
+alice2 = [mapping[v] for v in alice]
+print alice2[:100]
